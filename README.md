6e4c3aa62a0f4a723ea9df0a39c4803c9b52fe98
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-03-06 21:16:54 -0500

    Add data persistence with a database

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # chat-web
+
+## Data base
+
+### Install Turso CLI
+
+Seguir los pasos de la documentación oficial de [Turso](https://docs.turso.tech/cli/installation).
+
+### Create a new database
+
+```bash
+turso db create chat-web
+```
+
+### Initial steps
+
+Start an interactive SQL shell with:
+
+```bash
+turso db shell chat-web
+```
+
+To see information about the database, including a connection URL, run:
+
+```bash
+turso db show chat-web
+```
+
+To get an authentication token for the database, run:
+
+```bash
+turso db tokens create chat-web
+```
+
+### Install the necessary dependencies
+
+```bash
+npm install @libsql/client dotenv
+```
--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client' // importar el cliente de libsql para conectarse a la base de datos
+
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+import { Console } from 'node:console'
+
+dotenv.config() // carga las variables de entorno desde el archivo .env
 
 const port = process.env.PORT ?? 3000
 
@@ -11,16 +17,57 @@ const io = new Server(server, {
   connectionStateRecovery: true, // habilita la recuperación de estado de conexión
 }) // io -> in and out
 
-io.on('connection', (socket) => {
+const db = createClient({
+  url: "libsql://chat-web-yesiddn.turso.io", // url de la base de datos
+  authToken: process.env.DB_TOKEN, // token de autenticación
+})
+
+await db.execute(`
+CREATE TABLE IF NOT EXISTS messages(id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT)`) // crea la tabla si no existe
+
+io.on('connection', async (socket) => {
   console.log('an user has connected')
 
   socket.on('disconnect', () => {
     console.log('an user has disconnected')
   })
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg) // envia el mensaje a todos los usuarios conectados
+  socket.on('chat message', async (msg) => {
+    let result
+    try {
+      result = await db.execute(
+        {
+          sql: 'INSERT INTO messages(message) VALUES(:msg)',
+          args: {
+            msg
+          }
+        }
+      )
+    } catch (error) {
+      console.error(error)
+      return
+    }
+
+    io.emit('chat message', msg, result.lastInsertRowid.toString()) // envia el mensaje a todos los usuarios conectados y además el id del mensaje
   })
+
+  // console.log('auth: ', socket.handshake.auth) // imprime los datos de autenticación del socket (si es que hay alguno
+
+  if (!socket.recovered) { // recuperar mensajes que se han enviado despues de perder la conexión
+    try {
+      const results = await db.execute({
+        sql: `SELECT * FROM messages WHERE id > ?`,
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.message, row.id.toString()) // envia el mensaje al usuario que se ha reconectado
+      })
+    } catch (error) {
+      console.error(error)
+      return
+    }
+  }
 })
 
 app.use(logger('dev')) // logger de http request en consola
